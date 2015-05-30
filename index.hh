d5cac428991c1a82f63d6b7edcd368fa7ca2adf5
tchapi <regbasket@gmail.com>
2015-05-30 18:58:06 +0200

    Fixups

--- index.hh
+++ index.hh
@@ -3,7 +3,13 @@
 require "config.hh";
 require "GenerativeApiArt.hh";
 
+if ($_GET['q'] === "bitmap"){
+  $mode = GenerativeApiArt::MODE_BITMAP;
+} else {
+  $mode = GenerativeApiArt::MODE_COLOR;
+}
+
 $test = new GenerativeApiArt(Config::KEY, 500);
-$test->call()->extract()->create(GenerativeApiArt::MODE_BITMAP);
+$test->call()->extract()->create($mode);
 
 $test->output();
